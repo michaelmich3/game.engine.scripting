51dd1673398cc1f73224d6fe0f1c68fc4a3a7514
michaelmich3 <michaelmich3@hotmail.com>
2017-09-18 15:27:55 -0500

    Followed along with class

---
+++ GES 2D Demo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 0.1f;
+    public float jumpForce = 2;
+    private bool collisionCheck;
+
+    void Start ()
+    {
+
+	}
+	
+	void Update ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(speed * horizontalInput, 0, 0);
+
+        if (Input.GetButtonDown("Jump") & collisionCheck == true)
+        {
+            transform.Translate(0, jumpForce, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collisionCheck = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collisionCheck = false;
+    }
+}
