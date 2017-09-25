486a14961b28a9a8cb64d289969f17bc1f2919b1
michaelmich3 <michaelmich3@hotmail.com>
2017-09-25 15:20:21 -0500

    Fixes
    Fixed moving and jumping

--- GES 2D Demo/Assets/Scripts/PlayerMovement.cs
+++ GES 2D Demo/Assets/Scripts/PlayerMovement.cs
@@ -2,25 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-
-    public float speed = 0.1f;
-    public float jumpForce = 2;
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody2D;
+    [SerializeField]
+    private float speed = 5;
+    [SerializeField]
+    private float jumpForce = 5;
     private bool collisionCheck;
 
     void Start ()
     {
-
+        myRigidBody2D = GetComponent<Rigidbody2D>();
 	}
 	
 	void Update ()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(speed * horizontalInput, 0, 0);
 
+        if (collisionCheck == true)
+        {
+            myRigidBody2D.velocity = new Vector2(speed * horizontalInput, myRigidBody2D.velocity.y);
+        }
+        /*else
+        {
+             myRigidBody2D.velocity += new Vector2((speed/10) * horizontalInput, 0);
+        }
+        */
+        
         if (Input.GetButtonDown("Jump") & collisionCheck == true)
         {
-            transform.Translate(0, jumpForce, 0);
+            myRigidBody2D.velocity = new Vector2(myRigidBody2D.velocity.x, jumpForce);
+            collisionCheck = false;
         }
     }
 
